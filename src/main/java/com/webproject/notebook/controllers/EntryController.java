e36d40adab1d6387113afa694e565a2773d0175e
DmitryMalorodov <114594637+DmitryMalorodov@users.noreply.github.com>
2024-01-06 13:58:03 +0300

    - подключение базы данных - создание сущности "записи" - добавление в контроллер записей, обработку получения всех записей из базы данных - добавление в шаблон записей (html файл), обработку получения данных из котроллера записей

--- src/main/java/com/webproject/notebook/controllers/EntryController.java
+++ src/main/java/com/webproject/notebook/controllers/EntryController.java
@@ -1,5 +1,8 @@
 package com.webproject.notebook.controllers;
 
+import com.webproject.notebook.models.Entry;
+import com.webproject.notebook.repo.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EntryController {
 
+    @Autowired
+    private EntryRepository entryRepository;
+
     @GetMapping("/entry")
     public String entryMain(Model model) {
+        Iterable<Entry> entries = entryRepository.findAll();
+        model.addAttribute("entries", entries);
         return "entryMain";
     }
 }
